51b0c3e03ff55f450067b7fa4ba7a8062c9e6aba
Andras Sevcsik <sevcsik@sevdev.hu>
2018-04-08 03:29:50 +0200

    Added initial implementation for Maybe

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-export class Foo {
-	static bar(): number {
-		return 5
-	} 
+import { IMonad, IMonadStatic, Maybe } from "monet"
+
+export function genericDo<M extends IMonad<any>>(monad: IMonadStatic, co: () => IterableIterator<M>): M {
+	function isNotDone(el: IteratorResult<M> | IteratorResult<any>): el is IteratorResult<M> { return !el.done }
+
+	const it: IterableIterator<M> = co()
+	const iterate: (el: IteratorResult<M> | IteratorResult<any>) => M = el =>
+		(isNotDone(el) ? el.value.bind((val: any) => iterate(it.next(val))) : monad.unit(el.value)) as M
+
+	return iterate(it.next())
 }
+
+export function maybeDo(co: () => IterableIterator<Maybe<any>>): Maybe<any> { return genericDo(Maybe, co) }
+
